08d042e7897be473e4b5ca53cddfae8cc976debc
ggomersall <ggomersall@gmail.com>
2015-12-13 18:01:32 +0000

    modified the app.js file, added config file for routes, started user model

--- api/app.js
+++ api/app.js
@@ -12,8 +12,9 @@ var expressJWT     = require('express-jwt');
 var app            = express();
 
 
-var config = require ('./config/config');
 var User = require('./models/user');
+
+var secret = process.env.DEVIGNER_APP_SECRET;
 
 
 // connecting the DB
@@ -24,7 +25,7 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb:/localhost:27017/devigner-
 app.use(methodOverride(function(req, res){
   if(req.body && typeof req.body === 'object' && '_method' in req.body) {
     var method = req.body._method
-    delete.req.body._method
+    delete req.body._method
     return method
   };
 }));
---
+++ api/config/routes.js
@@ -0,0 +1,21 @@
+var express     = require('express');
+var router      = express.Router();
+var passport    = require('passport');
+
+var usersController = require('../controllers/usersController');
+var authController = require('../controllers/authController');
+
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
+
+router.route('/users')
+  .get(usersController.usersIndex)
+
+router.route('/users/:id')
+  .get(usersController.usersShow)
+  .put(usersController.usersUpdate)
+  .patch(usersController.usersUpdate)
+  .delete(usersController.usersDelete)
+
+
+module.exports = router
