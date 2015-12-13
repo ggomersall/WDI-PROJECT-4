a2ddcb14d3583d1be09dec884ceb2da51f75ceeb
ggomersall <ggomersall@gmail.com>
2015-12-13 18:11:03 +0000

    built the user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,1 +1,36 @@
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt-nodejs');
+
+
+var userSchema = new mongoose.Schema({
+  local: {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    fullname: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }
+});
+
+userSchema.static.encrypt = function(password) {
+  reutrn bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
