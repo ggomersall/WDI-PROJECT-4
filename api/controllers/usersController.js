7ba332c4287b4e6641995b3478e02d1c47f114c5
Gareth Gomersall <ggomersall@gmail.com>
2015-12-14 09:47:07 +0000

    changed the some modules in usersController to singular

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -7,14 +7,14 @@ function usersIndex(req, res) {
   });
 };
 
-function usersShow(req, res) {
+function userShow(req, res) {
   User.findById(req.params.id, function(err, user) {
     if (err) return res.status(404).json({ message: "Ooops,  something went wrong." });
     res.status(200).json({ user: user });
   });
 };
 
-function usersUpdate(req, res) {
+function userUpdate(req, res) {
   User.findById(req.params.id, function(err, user) {
     if(err) return res.status(500).json({ message: "Ooops,  something went wrong." });
     if(!user) return res.status(404).json({ message: "That user could not be found!" });
@@ -33,7 +33,7 @@ function usersUpdate(req, res) {
   });
 };
 
-function usersDelete(req, res) {
+function userDelete(req, res) {
   User.findByIdAndRemove({_id: req.params.id}, function(err) {
     if(err) return res.status(404).json({ message: "Ooops,  something went wrong." });
     res.status(200).json({ message: "User has been deleted!"});
@@ -44,7 +44,7 @@ function usersDelete(req, res) {
 
 module.exports = {
   usersIndex: usersIndex,
-  usersShow: usersShow,
-  usersUpdate: usersUpdate,
-  usersDelete: usersDelete
+  userShow: userShow,
+  userUpdate: userUpdate,
+  userDelete: userDelete
 }
