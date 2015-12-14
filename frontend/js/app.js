2715fb2081c433f4e10f47a40f19e57f419b1b84
Gareth Gomersall <ggomersall@gmail.com>
2015-12-14 12:20:05 +0000

    edited app.js to include the mainRouter and ui-angular ofr partials, created views folder with the login.html

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,41 @@
 angular
-  .module('devigner', ['angular-jwt', 'ngResource'])
+  .module('devigner', ['angular-jwt', 'ui.router', 'ngResource'])
   .constant('API', 'http://localhost:3000/api')
   .config(function($httpProvider){
     $httpProvider.interceptors.push('authInterceptor');
   })
+  .config(MainRouter)
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+      .state('home', {
+        url: "/",
+        templateUrl: "partials/home.html",
+      })
+      .state('products', {
+        url: "/products",
+        templateUrl: "partials/products.html",
+      })
+      .state('viewProduct', {
+        url: "/products/:id",
+        templateUrl: "partials/product.html",
+        controller: 'ProductViewController'
+      })
+      .state('about', {
+        url: "/about",
+        templateUrl: "partials/about.html",
+      })
+      .state('login', {
+        url: "/login",
+        templateUrl: "partials/login.html"
+      })
+      .state('register', {
+        url: "/register",
+        templateUrl: "partials/register.html",
+      })
+      .state('cart', {
+        url: "/cart",
+        templateUrl: "partials/cart.html",
+      });
+}
