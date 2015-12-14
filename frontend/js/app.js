f8216386311bacc81ccc27b050d85bfd8c585881
Gareth Gomersall <ggomersall@gmail.com>
2015-12-14 14:57:29 +0000

    added cdn for ng-resource

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,32 +1,33 @@
 angular
   .module('devigner', ['angular-jwt', 'ui.router', 'ngResource'])
   .constant('API', 'http://localhost:3000/api')
+  .config(MainRouter)
   .config(function($httpProvider){
     $httpProvider.interceptors.push('authInterceptor');
-  })
-  .config(MainRouter)
+  });
+  
 
 MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
-      .state('home', {
-        url: "/",
-        templateUrl: "views/home.html"
-      })
-      .state('users', {
-        url: "/users",
-        templateUrl: "views/users.html"
-      })
-      .state('about', {
-        url: "/about",
-        templateUrl: "views/about.html"
-      })
-      .state('login', {
-        url: "/login",
-        templateUrl: "views/login.html"
-      })
-      .state('signup', {
-        url: "/signup",
-        templateUrl: "views/signup.html"
-      })
+    .state('home', {
+      url: "/",
+      templateUrl: "views/home.html"
+    })
+    .state('users', {
+      url: "/users",
+      templateUrl: "views/users.html"
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "views/about.html"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/login.html"
+    })
+    .state('signup', {
+      url: "/signup",
+      templateUrl: "views/signup.html"
+    })
 }
